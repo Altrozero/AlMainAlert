a67de69b0db3fa016fda59da56ca66e1fee36d58
Timothy <altrozero@gmail.com>
2018-04-23 21:20:51 +0100

    Changed alSiteAlert-holder to fixed position, default on most sites where originally used.

--- alMainAlert/alMainAlert.js
+++ alMainAlert/alMainAlert.js
@@ -1,10 +1,10 @@
 /**
  * Simple jQuery plugin for showing alerts and errors on a page.
- * 
+ *
  * @author Timothy Wilson 2018 <https://github.com/Altrozero>
- * 
+ *
  * Thanks to; http://stefangabos.ro/jquery/jquery-plugin-boilerplate-revisited/
- * 
+ *
  * ******************************
  * SETTINGS
  * ******************************
@@ -12,7 +12,7 @@
  * class string "The style you want to use"
  * type [overlay|block] "overlay is the default, you can use block when you dont want it to cover content.
  *    It's suggested you don't mixed and match both in a project."
- *  
+ *
  */
 
 (function($) {
@@ -29,20 +29,20 @@
 
     plugin.init = function() {
       plugin.settings = $.extend({}, defaults, options);
-      
+
       // Add the holder for hoverovers
       if (!$("#alSiteAlert-holder").length) {
         jQuery("<div/>", {
           id: "alSiteAlert-holder",
           css: {
-            position: "absolute",
+            position: "fixed",
             top: "0px",
             right: "0px",
             left: "0px"
           }
         }).appendTo("body");
       }
-      
+
       // Add the holder for dropdowns
       if (!$("#alSiteAlert-holderBlock").length) {
         jQuery("<div/>", {
@@ -72,7 +72,7 @@
           }
         })
       );
-    
+
       switch(plugin.settings.type) {
         case "block":
           holder.appendTo("#alSiteAlert-holderBlock");
